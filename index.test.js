f32c1bbcdd372a06918e8125a06c816414939bda
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2024-11-25 16:36:14 -0600

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /', () => {
-  it('should respond with "Hola amigos desde mi API 2"', async () => {
+  it('should respond with "Hola amigos desde mi API 3"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hola amigos desde mi API 2');
+    expect(response.text).toBe('Hola amigos desde mi API 3');
   });
 });
